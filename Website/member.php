9ee97eac7e4ce8ecf6197d32c66eeff2f5d6d2a2
james08m <james08major@gmail.com>
2015-03-12 00:30:58 -0400

    Website: Create member section

---
+++ Website/member.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: J08M
+ * Date: 3/11/2015
+ * Time: 11:40 PM
+ */
+
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Virtualgaze | Membre</title>
+    <meta name="description" content="Virtualgaze est un logiciel de surveillance d'ordinateur qui vous permet de garder un oeil sur vos appareils à partir de votre compte." />
+    <meta name="keywords" content="" />
+    <meta property="og:title" content="Virtualgaze | Membre" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="virtualgaze.hol.es" />
+    <meta property="og:site_name" content="Virtualgaze" />
+    <meta property="og:description" content="Virtualgaze est un logiciel de surveillance d'ordinateur qui vous permet de garder un oeil sur vos appareils à partir de votre compte." />
+    <meta property="og:image" content="" />
+    <link rel="shortcut icon" href="" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+
+
+<body>
+    <?php include 'Includes/nav.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <ul class=" well nav nav-lists col-sm-2" style="border-radius: 0px; padding: none;">
+                <li class="nav-header"><span class="glyphicon glyphicon-th"></span> Ordinateurs</li>
+
+                <!-- Ordinateur -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-th-list"></span> Ordinateur 1<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User1</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User2</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User3</a></li>
+                    </ul>
+                </li>
+
+                <!-- Ordinateur -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-th-list"></span> Ordinateur 2<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User1</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User2</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User3</a></li>
+                    </ul>
+                </li>
+
+                <!-- Ordinateur -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-th-list"></span> Ordinateur 3<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User1</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User2</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> User3</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+
+            <div class="col-sm-10">
+                <div class="hero-unit">
+                    <h1>Hello, world!</h1>
+                    <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                    <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+                </div>
+                <div class="row-fluid">
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                </div><!--/row-->
+                <div class="row-fluid">
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                    <div class="span4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                </div><!--/row-->
+            </div><!--/span-->
+        </div><!--/row-->
+
+        <hr>
+</body>
+
